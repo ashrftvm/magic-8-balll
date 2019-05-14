e1a6fb9e37d27c66d94bef497e3b0c69d61d4fbe
Ashrf Ali <asifshatvm@gmail.com>
2019-05-14 23:48:45 +0400

    Completed the app using one statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Ask Me Anything!'),
+            backgroundColor: Colors.teal,
+          ),
+          body: EightBall(),
+        ),
       ),
     );
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  int ballNumber = 0;
+  void changeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (changeBall),
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
